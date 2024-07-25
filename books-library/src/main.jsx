df9fde0b4e3e107a317a39efc84e3e791b327ba5
umairgosal <umairgosal123@gmail.com>
2024-07-25 12:18:59 +0500

    added google analytics script to index.html file

--- books-library/src/main.jsx
+++ books-library/src/main.jsx
@@ -4,7 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/index'
+import TagManager from 'react-gtm-module'
 
+const tagManagerArgs = {
+    gtmId: 'GTM-YRW3FTE6PF'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
